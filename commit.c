5724b9e22d3a582d354dd176e25243a08758e8ac
CraftyChimera <viswanathiyyappan@gmail.com>
2021-09-05 14:37:57 +0530

    Removed memory leaks and replaced system() calls with C code

--- commit.c
+++ commit.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-char *word(char *a, int n)
+#include <openssl/sha.h>
+char rem[100][1000];
+counter = 0;
+void word(char *a, int n,char *b)
 {
-    int space[100];
-    char *ret = malloc(sizeof(char) * 100);
-    int k = 1, i;
+    int space[100],k=1,i;char ret[1000];
     space[0] = -1;
     for (i = 0; a[i] != '\0'; i++)
     {
@@ -16,48 +17,55 @@ char *word(char *a, int n)
     }
     k = 0;
     for (i = space[n - 1] + 1; a[i] != '\0' && i != space[n]; i++)
-        ret[k++] = a[i];
+        b[k++] = a[i];
     ret[k] = '\0';
-    return ret;
+    return;
 }
 
-char *hashb(char *a)
+void hashb(char *a,char *b)
 {
-    char *str = malloc(sizeof(char) * 256), read[256];
-    FILE *b;
-    sprintf(str, "/home/drake/xing/hash-app %s", a);
-    system(str);
-    b = fopen("a", "r+");
-    if (!b)
+    char str[2500];
+    FILE *fi;
+    fi = fopen(a, "r");
+    while (fgets(str, 256, fi));
+    fclose(fi);
+    SHA_CTX ctx;
+    SHA1_Init(&ctx);
+    SHA1_Update(&ctx, str, strlen(str));
+    unsigned char tmphash[SHA_DIGEST_LENGTH];
+    SHA1_Final(tmphash, &ctx);
+    int i = 0;
+    for (i = 0; i < SHA_DIGEST_LENGTH; i++)
     {
-        return a;
+        sprintf((char *)&(b[i * 2]), "%02x", tmphash[i]);
     }
-    fgets(str, 256, b);
-    fclose(b);
-    remove("a");
-    return word(str, 1);
+    return;
 }
 
-char *fakehashb(char *a)
-{
-    char *str = malloc(sizeof(char) * 256), read[256];
-    FILE *b;
-    sprintf(str, "echo %s > x;sha1sum x>a", a);
-    system(str);
-    b = fopen("a", "r+");
-    fgets(str, 256, b);
-    fclose(b);
-    remove("a");
-    remove("x");
-    return word(str, 1);
+void fakehashb(char *a,char *b)
+{   SHA_CTX ctx;
+    SHA1_Init(&ctx);
+    SHA1_Update(&ctx, a, strlen(a));
+    unsigned char tmphash[SHA_DIGEST_LENGTH];
+    SHA1_Final(tmphash, &ctx);
+    unsigned char *hash = malloc(sizeof(char) * SHA_DIGEST_LENGTH * 2);
+    int i = 0;
+    for (i = 0; i < SHA_DIGEST_LENGTH; i++)
+    {
+        sprintf((char *)&(b[i * 2]), "%02x", tmphash[i]);
+    }
+    strcpy(rem[counter++],b);
+    return ;
 }
 
 int depth(char *a)
 {
     int j = 0;
     for (int i = 0; a[i] != '\0'; i++)
-        {if (a[i] == '/')
-            j++;}
+    {
+        if (a[i] == '/')
+            j++;
+    }
     return j;
 }
 
@@ -81,59 +89,64 @@ void separate(char *a, char *b)
 
 int main()
 {
-    FILE *x = fopen("index", "r+"), *y;
-    char m[300], hash[1000][200], string[1000][200], tail[1000][200], fakehash[1000][200];
-    int i = 0, count = 0, d[1000], maxd = 0,file[1000];
+    FILE *x = fopen(".xing/index", "r+"), *y;
+    char m[3000], hash[10000][200], string[10000][200], tail[10000][200], fakehash[10000][200];
+    int i = 0, count = 0, d[1000], maxd = 0, file[1000];
     while (fgets(m, 100, x))
     {
-        strcpy(hash[i], word(m, 1));
-        free(word(m, 1));
-        strcpy(string[i], word(m, 2));
-        free(word(m, 2));
+        word(m, 1,hash[i]);
+        word(m, 2,string[i]);
         d[i] = depth(string[i]);
-        if(maxd<d[i])maxd=d[i];
-        file[i]=1;
+        if (maxd < d[i])
+            maxd = d[i];
+        file[i] = 1;
         i++;
     }
     count = i;
     for (i = 0; i < count; i++)
-        {
-            separate(string[i], tail[i]);
-            strcpy(fakehash[i], fakehashb(string[i]));
-            y = fopen(fakehash[i], "a");
-            fprintf(y, "node %s %s", hash[i], tail[i]);
-            fclose(y);
-        }
+    {
+        separate(string[i], tail[i]);
+        fakehashb(string[i],fakehash[i]);
+        y = fopen(fakehash[i], "a");
+        fprintf(y, "node %s %s", hash[i], tail[i]);
+        fclose(y);
+       }
+
     for (i = 0; i < count; i++)
         if (d[i] == maxd)
-        {   file[i]=0;
-            sprintf(m,"cp %s %s",fakehash[i],hashb(fakehash[i]));
-             system(m); strcpy(hash[i], hashb(fakehash[i]));
-
+        {
+            file[i] = 0;
+            hashb(fakehash[i],hash[i]);
+            sprintf(m, "cp %s %s", fakehash[i], hash[i]);
+            system(m);
         }
 
     while (maxd)
     {
         for (i = 0; i < count; i++)
         {
-            if (d[i] == maxd && file[i]==0)
-            {   separate(string[i], tail[i]);
-                strcpy(fakehash[i], fakehashb(string[i]));
-                sprintf(m,"/home/drake/xing/commit.sh %s %s %s",hash[i],tail[i],fakehash[i]);
+            if (d[i] == maxd && file[i] == 0)
+            {
+                separate(string[i], tail[i]);
+                fakehashb(string[i],fakehash[i]);
+                sprintf(m, "/home/drake/xing/commit.sh %s %s %s", hash[i], tail[i], fakehash[i]);
                 system(m);
-            }
+                }
         }
 
         for (i = 0; i < count; i++)
         {
             if (d[i] == maxd)
-            {sprintf(m,"cp %s %s",fakehash[i],hashb(fakehash[i]));
-             system(m);
-            strcpy(hash[i], hashb(fakehash[i]));   
-                d[i]--;file[i]=0;
-            }
+            {  hashb(fakehash[i],hash[i]);
+                sprintf(m, "cp %s %s", fakehash[i],hash[i]);
+                system(m);
+                d[i]--;
+                file[i] = 0;
+                }
         }
         maxd--;
     }
+    for (int i = 0; i < counter; i++)
+        remove(rem[i]);
     return 0;
 }
